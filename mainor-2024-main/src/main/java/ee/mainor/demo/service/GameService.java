281a1400b0f4be0a315ef358c55699603dbf27bc
Alexander Böttger <scienceboeddi@outlook.com>
2024-03-12 19:17:11 +0100

    update

---
+++ mainor-2024-main/src/main/java/ee/mainor/demo/service/GameService.java
@@ -0,0 +1,25 @@
+import org.springframework.stereotype.Service;
+import java.util.Random;
+
+@Service
+public class GameService {
+    private int randomNumber;
+    private Random random = new Random();
+
+    public void generateNewNumber() {
+        this.randomNumber = random.nextInt(100) + 1;
+    }
+
+    public String checkGuess(int guess) {
+        if (guess < 1 || guess > 100) {
+            return "Guess must be between 1 and 100.";
+        }
+        if (guess < randomNumber) {
+            return "Higher";
+        } else if (guess > randomNumber) {
+            return "Lower";
+        } else {
+            return "Correct! The number was " + randomNumber;
+        }
+    }
+}
